2b27e3c2f4f26d34152afd822f2540002a58ba82
Maryia Varabyeva <maryia.varabyeva@gmail.com>
2022-07-12 12:34:29 +0300

    feat: add basic project settings

---
+++ virtual-keyboard/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "Virtual keyboard",
+    filename: 'index.html',
+      template: 'src/index.html',
+      favicon: "./src/assets/img/keyboard.png"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
